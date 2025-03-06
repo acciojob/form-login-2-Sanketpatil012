d2b05fe76b9467c4456487ef7478a34fe6477007
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-06 16:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+let btn= document.getElementById("btn")
+
+btn.addEventListener('click' , ()=>{
+    event.preventDefault()
+	let firstname= document.getElementById('name').value 
+	let  lastname= document.getElementById('last_name').value
+	let  phoneNumber= document.getElementById('number').value
+	let emailid = document.getElementById('email').value
+	alert(`First Name:${firstname} Last Name:${lastname} Phone Number:${phoneNumber} Email ID:${emailid}   `)
+})
+
+
+
